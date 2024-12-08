1b7875564e1aac81c1f1fd9dd70f16fc0b8e9124
donjd <99505859+donjd@users.noreply.github.com>
2024-12-08 16:00:04 -0700

    Experiment with different eventListeners

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-const buttons = document.querySelectorAll("button");
+const output = document.querySelector("#output");
+const container = document.querySelector("#container");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
+function handleClick(e) {
+  switch (e.target.id) {
+    case "red":
+      console.log("you clicked on the red button");
+      break;
+    case "green":
+      console.log("you clicked on the green button");
+      break;
+    case "blue":
+      console.log("you clicked on the blue button");
+      break;
+  }
+}
+
+container.addEventListener("click", handleClick);
+
+const canvas = document.querySelector(".canvas");
+const coordinates = document.querySelector("#coordinates");
+
+canvas.addEventListener("keypress", (e) => {
+  coordinates.textContent = `X: ${e.clientX}, Y: ${e.clientY}`;
+  let keys = [];
+
+  if (e.shiftKey) keys.push(`shift`);
+  if (e.ctrlKey) keys.push(`ctrl`);
+  if (e.altKey) keys.push(`alt`);
+  if (e.metaKey) keys.push(`meta`);
+
+  const whichKey = document.querySelector("#which-key");
+  whichKey.textContent = `Keys: ${keys.join("+")}`;
 });
